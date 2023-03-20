efe4241771439eb93a6bee8bf02f496f2dbace83
Pedro Carpe <dd2426016@gmail.com>
2023-03-20 19:27:40 -0300

    Algoritmo de Calculo de Tabuada

---
+++ src/CalculaTabuadas.java
@@ -0,0 +1,15 @@
+
+public class CalculaTabuadas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int contador=1;contador<=10;contador++){
+			System.out.println("");
+			System.out.println("Tabuada "+contador);
+			System.out.println("------------");
+			for(int contadora=1;contadora<=10;contadora++){
+				System.out.println(contador+" * "+contadora+" = "+contador*contadora);	
+				}
+			}
+	}
+}
